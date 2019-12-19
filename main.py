d90104c5b3009e652faabb4419f487aab4ddfb7e
ilanlin <710057201@qq.com>
2019-12-19 15:50:21 +0800

    按结构修改代码
    按结构修改代码

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello")
+#coding=utf-8  
+__author__ = 'cloudtogo'
+
+from flask import Flask
+import sys
+
+
+app = Flask(__name__)
+
+
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+
+print("test nono")
+@app.route('/')
+def hello():
+    return '恭喜您成功地发布了一个运行在云端的python程序 hello'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
